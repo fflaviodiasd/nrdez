50bb5947844384e4fe4c13e46abba607d01fe514
ffsilva <ffsilva@sfiec.org.br>
2024-04-12 17:24:41 -0300

    Tabela companies

--- apps/companies/admin.py
+++ apps/companies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from apps.companies.models import Company
 # Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cnpj', 'email', 'telefone' )
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'cnpj')
+    list_per_page = 20
+
+admin.site.register(Company, CompanyAdmin)
--- apps/companies/models.py
+++ apps/companies/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    name = models.CharField(max_length=30)
+    nome = models.CharField(max_length=30)
     cnpj = models.CharField(max_length=30, blank=True)
-    email = models.EmailField()
+    email = models.EmailField(max_length=30, blank=True)
     telefone = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
